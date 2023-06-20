10eb631a529ac46cb1a79e211a8f6d0f971a3d1b
MarouanSidali <marwandx57@gmail.com>
2023-06-20 10:15:13 +0000

    Added items to local storage with useEffect hook

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -1,27 +1,34 @@
-import { useState } from "react";
-import Form from './Form'
+import { useEffect, useState } from "react";
+import Form from "./Form";
 import "./styles.css";
 import TodoList from "./TodoList";
 
 const App = () => {
-  
-  const [todos, setTodos] = useState([]);
-  
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return [];
+
+    return JSON.parse(localValue);
+  });
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
+
   const toggleTodo = (id, completed) => {
     setTodos((currentTodos) => {
-      return currentTodos.map(todo => {
-        if(todo.id === id) {
-          return {...todo, completed}
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
         }
-        return todo
-      })
-    })
-  }
+        return todo;
+      });
+    });
+  };
   const deleteTodo = (id) => {
-    setTodos(currentTodos => {
-      return currentTodos.filter((todo) => todo.id !== id )
-    })
-  }
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  };
   const addTodo = (title) => {
     setTodos((currentTodos) => {
       return [
@@ -29,12 +36,12 @@ const App = () => {
         { id: crypto.randomUUID(), title, completed: false },
       ];
     });
-  }
+  };
   return (
     <>
       <Form onSubmit={addTodo} />
       <h1 className="header">Todo List</h1>
-      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
   );
 };
